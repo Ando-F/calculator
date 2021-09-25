bf0ccd1d6df6939a32e4604fe7d72f70393853a9
matveisch <matvesch@protonmail.com>
2021-09-25 19:43:36 +0400

    c5

--- script.js
+++ script.js
@@ -18,33 +18,60 @@ function operate(num1, num2, operator) {
     return operator(num1, num2);
 };
 
-calcMemory = {};
+const displayInput = document.getElementById('displayId');
+const numButtons = document.querySelectorAll('.numbers');
+const operateButtons = document.querySelectorAll('.operators');
 
-const calcDisplay = document.querySelector('h1');
+let calcMemory = {};
 
-const nineButton = document.querySelector('.nine');
-const eightButton = document.querySelector('.eight');
+let clicked = false;
 
-nineButton.addEventListener('click', () => {
-    calcDisplay.textContent += 9;
+numButtons.forEach((button) => {
+    let buttonInnerHTML = button.innerHTML;
+    button.addEventListener('click', () => {
+        displayInput.value += buttonInnerHTML;
+        if (clicked == true) {
+            displayInput.value = "";
+            displayInput.value += buttonInnerHTML;
+            calcMemory.number2 = parseFloat(displayInput.value);
+            clicked = false;
+        };
+    });
 });
 
-eightButton.addEventListener('click', () => {
-    calcDisplay.textContent += 8;
+operateButtons.forEach((button) => {
+    let buttonInnerHTML = button.innerHTML;
+    button.addEventListener('click', () => {
+        switch (buttonInnerHTML) {
+            case '+' :
+                calcMemory.number1 = parseFloat(displayInput.value);
+                calcMemory.operator = add;
+                clicked = true;
+            break;
+            case '-' :
+                calcMemory.number1 = parseFloat(displayInput.value);
+                calcMemory.operator = subtract;
+                clicked = true;
+            break;
+            case '/' :
+                calcMemory.number1 = parseFloat(displayInput.value);
+                calcMemory.operator = divide;
+                clicked = true;
+            break;
+            case '*' :
+                calcMemory.number1 = parseFloat(displayInput.value);
+                calcMemory.operator = multiply;
+                clicked = true;
+                break;
+            case '=' :
+                console.log(calcMemory);
+                displayInput.value = operate(calcMemory.number1, calcMemory.number2, calcMemory.operator);
+                clicked = true;
+            break;
+            case 'AC' :
+                displayInput.value = "";
+                calcMemory = {};
+            break;
+        };
+    });
 });
-
-const plusButton = document.querySelector('.plus');
-const equalsButton = document.querySelector('.equals');
-
-plusButton.addEventListener('click', () => {
-    calcMemory.num1 = parseInt(calcDisplay.textContent);
-    calcMemory.operator = add;
-    calcDisplay.textContent = "";
-});
-
-equalsButton.addEventListener('click', () => {
-    calcMemory.num2 = parseInt(calcDisplay.textContent);
-    console.log(calcMemory);
-    calcDisplay.textContent = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
-});
-
