2ef7bd5c5d395ce61c41523294853f0b8d95b27f
matveisch <matvesch@protonmail.com>
2021-09-27 21:25:32 +0400

    a2

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return parseFloat(num1) + parseFloat(num2);
 };
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return parseFloat(num1) - parseFloat(num2);
 };
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return parseFloat(num1) * parseFloat(num2);
 };
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return parseFloat(num1) / parseFloat(num2);
 };
 
 function operate(num1, num2, operator) {
@@ -24,23 +24,30 @@ const operateButtons = document.querySelectorAll('.operators');
 const handlersButtons = document.querySelectorAll('.handlers');
 
 let calcMemory = {
-    num1: null,
-    num2: null,
-    operator: null
+    num1: "",
+    num2: "",
+    operator: "",
+    sum: ""
 };
 
 numButtons.forEach((button) => {
     let buttonInnerHTML = button.innerHTML;
     button.addEventListener('click', () => {
-        // Выводим на экран калькулятора цифры с клавиатуры
-        displayInput.value += buttonInnerHTML;
+        // Если в памяти нет оператора, с клавиатуры вписываем в память 
+        // калькулятора первое число, потом выводим его на дисплей.
+        if (calcMemory.operator == "") {
+            calcMemory.num1 += buttonInnerHTML;
+            displayInput.value = calcMemory.num1;    
+        };
 
-        // После того как была нажата кнопка оператора, сохранить число 
-        // с дисплея как num2 в память калькулятора
-        if (calcMemory.operator != null) {
-            calcMemory.num2 = parseFloat(displayInput.value);
+        // Если в памяти нет оператора, то очистить дисплей, сохранить 
+        // введенное с клавиатуры число в память и вывести это число.
+        if (calcMemory.operator != "") {
+            displayInput.value = "";
+            calcMemory.num2 += buttonInnerHTML;
+            displayInput.value = calcMemory.num2;
             console.log(calcMemory);
-        }
+        };
     });
 });
 
@@ -49,26 +56,54 @@ operateButtons.forEach((button) => {
     button.addEventListener('click', () => {
         switch (buttonInnerHTML) {
             case '+':
-                calcMemory.operator = add;
-
-                // Если в памяти не сохранена num2, то сохранить num1
-                if (calcMemory.num2 == null) {
-                    calcMemory.num1 = parseFloat(displayInput.value);
-                };
-        
-                // Если была нажата кнопка оператора, то очистить дисплей калькулятора
-                if (calcMemory.operator != null) {
-                    displayInput.value = "";
-                };
-                console.log(calcMemory);
-                
                 // Если в памяти сохранены оператор и второе число, то при нажатии на
-                // оператор произвести вычисление. Результат сохраняется в первую 
-                // цифру
-                if (calcMemory.num2 != null && calcMemory.operator != null) {
-                    displayInput.value = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
-                    calcMemory.num1 = parseFloat(displayInput.value);
+                // оператор произвести вычисление. Результат сохраняется в первое 
+                // число, потом выводится на дисплей. Также очищается второе число.
+                if (calcMemory.num2 != "" && calcMemory.operator != "") {
+                    calcMemory.num1 = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
+                    displayInput.value = calcMemory.num1;
+                    calcMemory.num2 = "";
+                    calcMemory.operator = "";
                 };
+
+                calcMemory.operator = add;
+            break;
+            case '-': 
+                // Если в памяти сохранены оператор и второе число, то при нажатии на
+                // оператор произвести вычисление. Результат сохраняется в первое 
+                // число, потом выводится на дисплей. Также очищается второе число.
+                if (calcMemory.num2 != "" && calcMemory.operator != "") {
+                    calcMemory.num1 = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
+                    console.log(calcMemory)
+                    displayInput.value = calcMemory.num1;
+                    calcMemory.num2 = "";
+                };
+
+                calcMemory.operator = subtract;
+            break;
+            case '*':
+                // Если в памяти сохранены оператор и второе число, то при нажатии на
+                // оператор произвести вычисление. Результат сохраняется в первое 
+                // число, потом выводится на дисплей. Также очищается второе число.
+                if (calcMemory.num2 != "" && calcMemory.operator != "") {
+                    calcMemory.num1 = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
+                    displayInput.value = calcMemory.num1;
+                    calcMemory.num2 = "";
+                };
+
+                calcMemory.operator = multiply;
+            break;
+            case '/':
+                // Если в памяти сохранены оператор и второе число, то при нажатии на
+                // оператор произвести вычисление. Результат сохраняется в первое 
+                // число, потом выводится на дисплей. Также очищается второе число.
+                if (calcMemory.num2 != "" && calcMemory.operator != "") {
+                    calcMemory.num1 = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
+                    displayInput.value = calcMemory.num1;
+                    calcMemory.num2 = "";
+                };
+
+                calcMemory.operator = divide;
             break;
         };
     });
@@ -77,6 +112,26 @@ operateButtons.forEach((button) => {
 handlersButtons.forEach((button) => {
     let buttonInnerHTML = button.innerHTML;
     button.addEventListener('click', () => {
+        switch (buttonInnerHTML) {
+            case 'AC':
+                displayInput.value = "";
+                calcMemory = {
+                    num1: "",
+                    num2: "",
+                    operator: ""
+                };
+            break;
+            case '=':
+                calcMemory.sum = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
+                displayInput.value = calcMemory.sum;
+                calcMemory.num1 = "";
+                calcMemory.num2 = "";
+                calcMemory.operator = "";
 
+                if (calcMemory.operator != "") {
+                    calcMemory.num1 = calcMemory.sum; // почему число один не сохраняется в память? 
+                };
+            break;
+        };
     });
 });
