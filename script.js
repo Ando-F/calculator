c2d91002909f5cf8803e5fa6c717208fb870f2bd
matveisch <matvesch@protonmail.com>
2021-09-27 14:26:04 +0400

    a1

--- script.js
+++ script.js
@@ -21,69 +21,62 @@ function operate(num1, num2, operator) {
 const displayInput = document.getElementById('displayId');
 const numButtons = document.querySelectorAll('.numbers');
 const operateButtons = document.querySelectorAll('.operators');
+const handlersButtons = document.querySelectorAll('.handlers');
 
-let calcMemory = {};
-
-let clicked = false;
-let timesClicked = 0;
+let calcMemory = {
+    num1: null,
+    num2: null,
+    operator: null
+};
 
 numButtons.forEach((button) => {
     let buttonInnerHTML = button.innerHTML;
     button.addEventListener('click', () => {
+        // Выводим на экран калькулятора цифры с клавиатуры
         displayInput.value += buttonInnerHTML;
-        if (clicked == true) {
-            displayInput.value = "";
-            displayInput.value += buttonInnerHTML;
-            calcMemory.number2 = parseFloat(displayInput.value);
-            clicked = false;
-        };
+
+        // После того как была нажата кнопка оператора, сохранить число 
+        // с дисплея как num2 в память калькулятора
+        if (calcMemory.operator != null) {
+            calcMemory.num2 = parseFloat(displayInput.value);
+            console.log(calcMemory);
+        }
     });
 });
-
-function calculation() {
-    if (timesClicked < 1) {
-        calcMemory.number1 = parseFloat(displayInput.value);
-    };
-
-    clicked = true;
-    timesClicked++;
-
-    if (timesClicked > 1) {
-        displayInput.value = operate(calcMemory.number1, calcMemory.number2, calcMemory.operator);
-        calcMemory.number1 = parseFloat(displayInput.value);
-    };
-};
 
 operateButtons.forEach((button) => {
     let buttonInnerHTML = button.innerHTML;
     button.addEventListener('click', () => {
         switch (buttonInnerHTML) {
-            case '+' :
-                calculation();
+            case '+':
                 calcMemory.operator = add;
-            break;
-            case '-' :
-                calculation();
-                calcMemory.operator = subtract;
-            break;
-            case '/' :
-                calculation();
-                calcMemory.operator = divide;
-            break;
-            case '*' :
-                calculation();
-                calcMemory.operator = multiply;
-            break;
-            case '=' :
-                displayInput.value = operate(calcMemory.number1, calcMemory.number2, calcMemory.operator);
-                clicked = true;
-                timesClicked = 0;
-            break;
-            case 'AC' :
-                displayInput.value = "";
-                calcMemory = {};
-                timesClicked = 0;
+
+                // Если в памяти не сохранена num2, то сохранить num1
+                if (calcMemory.num2 == null) {
+                    calcMemory.num1 = parseFloat(displayInput.value);
+                };
+        
+                // Если была нажата кнопка оператора, то очистить дисплей калькулятора
+                if (calcMemory.operator != null) {
+                    displayInput.value = "";
+                };
+                console.log(calcMemory);
+                
+                // Если в памяти сохранены оператор и второе число, то при нажатии на
+                // оператор произвести вычисление. Результат сохраняется в первую 
+                // цифру
+                if (calcMemory.num2 != null && calcMemory.operator != null) {
+                    displayInput.value = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
+                    calcMemory.num1 = parseFloat(displayInput.value);
+                };
             break;
         };
+    });
+});
+
+handlersButtons.forEach((button) => {
+    let buttonInnerHTML = button.innerHTML;
+    button.addEventListener('click', () => {
+
     });
 });
