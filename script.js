891e57bbd1d048c3753bce2adf455806e607c7a5
matveisch <matvesch@protonmail.com>
2021-09-28 10:03:20 +0400

    a3

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ numButtons.forEach((button) => {
         };
 
         // Если в памяти нет оператора, то очистить дисплей, сохранить 
-        // введенное с клавиатуры число в память и вывести это число.
+        // введенное с клавиатуры второе число в память и вывести это число.
         if (calcMemory.operator != "") {
             displayInput.value = "";
             calcMemory.num2 += buttonInnerHTML;
@@ -56,6 +56,7 @@ operateButtons.forEach((button) => {
     button.addEventListener('click', () => {
         switch (buttonInnerHTML) {
             case '+':
+                console.log(calcMemory);
                 // Если в памяти сохранены оператор и второе число, то при нажатии на
                 // оператор произвести вычисление. Результат сохраняется в первое 
                 // число, потом выводится на дисплей. Также очищается второе число.
@@ -67,6 +68,7 @@ operateButtons.forEach((button) => {
                 };
 
                 calcMemory.operator = add;
+                console.log(calcMemory);
             break;
             case '-': 
                 // Если в памяти сохранены оператор и второе число, то при нажатии на
@@ -74,7 +76,6 @@ operateButtons.forEach((button) => {
                 // число, потом выводится на дисплей. Также очищается второе число.
                 if (calcMemory.num2 != "" && calcMemory.operator != "") {
                     calcMemory.num1 = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
-                    console.log(calcMemory)
                     displayInput.value = calcMemory.num1;
                     calcMemory.num2 = "";
                 };
@@ -122,15 +123,10 @@ handlersButtons.forEach((button) => {
                 };
             break;
             case '=':
-                calcMemory.sum = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
-                displayInput.value = calcMemory.sum;
-                calcMemory.num1 = "";
-                calcMemory.num2 = "";
+                calcMemory.num1 = operate(calcMemory.num1, calcMemory.num2, calcMemory.operator);
+                displayInput.value = calcMemory.num1;
                 calcMemory.operator = "";
-
-                if (calcMemory.operator != "") {
-                    calcMemory.num1 = calcMemory.sum; // почему число один не сохраняется в память? 
-                };
+                calcMemory.num2 = "";
             break;
         };
     });
